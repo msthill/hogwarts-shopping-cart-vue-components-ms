14ab08c8b3171f0cd3beb25d882695d47cd024db
msthill <micah.sthill123@gmail.com>
2026-03-13 19:26:01 -0500

    components intro

---
+++ src/components/CartList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <CartListItem></CartListItem>
+        <CartListItem></CartListItem>
+    </div>
+</template>
+
+<script setup>
+import CartListItem from '@/components/CartListItem.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CartTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Harry's Shopping Cart</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+        /* Styles for the cart title */
+        h1 {
+            padding: 20px;
+            max-width: 1200px;
+            margin: auto;
+        }
+</style>
